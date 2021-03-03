1d00c9d60bd526518e95adaa8f17a09ee54ceded
NicolaasKotze <33121021+NicolaasJKotze@users.noreply.github.com>
2021-03-03 21:56:13 +0200

    Created menus

--- source/scripts/level.gd
+++ source/scripts/level.gd
@@ -4,10 +4,10 @@ const TIMER_LIMIT = 2.0 # Print time intervals
 var timer = 0.0
 
 func _process(delta):
-    timer += delta
-    if timer > TIMER_LIMIT:
-        timer = 0.0
-        print("fps: " + str(Engine.get_frames_per_second()))
+	timer += delta
+	if timer > TIMER_LIMIT:
+		timer = 0.0
+		print("fps: " + str(Engine.get_frames_per_second()))
 
 func _ready():
 	get_node("HUD/btnMainMenu").connect("pressed", self, "_on_btnMainMenu_pressed")
